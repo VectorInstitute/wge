6d4562b5708d0e1469ba4eef904da7245bd07b6b
XkunW <marshallxkwang@gmail.com>
2022-07-19 11:52:59 -0400

    Increase model checkpoint freq. Use headless mode

--- wge/miniwob/instance.py
+++ wge/miniwob/instance.py
@@ -154,6 +154,7 @@ class MiniWoBInstance(Thread):
             options.add_argument('headless')
             options.add_argument('disable-gpu')
             options.add_argument('no-sandbox')
+            options.add_argument('--disable-dev-shm-usage')
         else:
             options.add_argument('app=' + self.url)
             options.add_argument('window-size={},{}'
